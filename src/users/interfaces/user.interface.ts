a2c6074844ebd42d16cd3d1749634eb45a0cda2c
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-05 13:10:32 +0600

    User interface implementation

---
+++ src/users/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+import * as mongoose from 'mongoose';
+
+export interface User extends mongoose.Document {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
