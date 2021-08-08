14027aa524971ded3d5aceaa88fbdfd0a4ca0ad2
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-08 15:02:31 +0600

    Schema update & input validation

--- src/users/model/user.model.ts
+++ src/users/model/user.model.ts
@@ -1,20 +1,45 @@
+import Joi from 'joi';
 import * as mongoose from 'mongoose';
 
-export const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+export const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+);
+
+export function validateInput(user) {
+  const schema = Joi.object({
+    firstName: Joi.string().min(5).max(50).required(),
+    lastName: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+    role: Joi.string().lowercase().valid('user', 'admin'),
+  });
+
+  return schema.validate(user);
+}
