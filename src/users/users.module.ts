421fefad2e51cb2ec72ed6ce4080ac1bccdc4f51
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-09 12:41:53 +0600

    Importing the JwtModule, PassportModule & providing JwtStrategy

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 
 import { userSchema } from 'src/users/model/user.model';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'kotha123',
+      signOptions: {
+        expiresIn: 300,
+      },
+    }),
+    MongooseModule.forFeature([{ name: 'User', schema: userSchema }]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtStrategy],
 })
 export class UsersModule {}
