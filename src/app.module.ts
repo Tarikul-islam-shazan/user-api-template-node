f5d0a7c39abba187e924d3f68ce74148932f95e7
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-17 11:45:38 +0600

    Database Connection using TypeORM module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,14 +11,22 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        autoIndex: false,
-      },
-    ),
+    // MongooseModule.forRoot(
+    //   `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`,
+    //   {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     autoIndex: false,
+    //   },
+    // ),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'user-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
