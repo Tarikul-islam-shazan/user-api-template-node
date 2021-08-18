4c2271f0ab5766bbf97807b912ffb0855bcbf140
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-18 11:07:21 +0600

    Role management, error handling and logger implementation

--- src/users/strategies/jwt.strategy.ts
+++ src/users/strategies/jwt.strategy.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { UsersRepository } from '../repositories/users.repository';
+import { User } from './../entities/user.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  logger = new Logger();
+
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {
     super({
       secretOrKey: 'kotha123',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
 
-  async validate(payload) {
-    return { userId: payload.id, userRole: payload.role };
+  async validate(payload: JwtPayload) {
+    const { id, role } = payload;
+    try {
+      const user: User = await this.usersRepository.findOne(id);
+
+      if (user.role !== 'admin') {
+        throw new UnauthorizedException('The user is not Authorized!');
+      }
+
+      const extractedUser = { userId: id, userRole: role };
+
+      this.logger.verbose(`The extracted user information: ${extractedUser}`);
+
+      return extractedUser;
+    } catch (err) {
+      this.logger.error('Unauthorized users security breach!', err.stack);
+      throw err;
+    }
   }
 }
