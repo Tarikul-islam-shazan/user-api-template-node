102a83c2bc349d27690db97c8d8910d6e2220baa
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-19 19:31:32 +0600

    Refactored the code & implemented the ConfigModule and ConfigService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { userSchema } from '../users/model/user.model';
 import { UsersController } from './controllers/users.controller';
 import { UsersRepository } from './repositories/users.repository';
 import { UsersService } from './services/users.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtStrategy } from './guards/jwt.strategy';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'kotha123',
-      signOptions: {
-        expiresIn: 300,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
     }),
-    // MongooseModule.forFeature([{ name: 'User', schema: userSchema }]),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
   controllers: [UsersController],
-  providers: [UsersService, JwtStrategy],
+  providers: [UsersService, JwtStrategy, ConfigService],
 })
 export class UsersModule {}
