a7a0fb60f9104474abacf6d0a6f0ed47e4cde25a
Md. Rubel Uddin Raihan <raihan.uits123@gmail.com>
2021-08-19 19:16:22 +0600

    Updated and solved issues

--- src/users/services/users.service.spec.ts
+++ src/users/services/users.service.spec.ts
@@ -3,16 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
 import { UsersRepository } from '../repositories/users.repository';
 import { UsersService } from './users.service';
-import { IsEmail } from 'class-validator';
 
 // const mockBcrypt = () => ({
-//     compare: jest.fn()
-// })
+//   compare: jest.fn(),
+// });
 
-const mockLogin = {
-  email: 'email',
-  password: 'password',
-};
+// const mockLogin = {
+//   email: 'email',
+//   password: 'password',
+// };
 
 const mockUser = {
   id: 'someId',
@@ -25,7 +24,7 @@ const mockUser = {
 const mockUsersRepository = () => ({
   getUsers: jest.fn(),
   getSingleUser: jest.fn(),
-  addUser: jest.fn(),
+  createUser: jest.fn(),
   updateUser: jest.fn(),
   deleteUser: jest.fn(),
   findOne: jest.fn(),
@@ -36,27 +35,28 @@ const mockJwtService = () => ({});
 describe('UsersService', () => {
   let usersService: UsersService;
   let usersRepository;
+  // let userBcrypt;
 
   beforeEach(async () => {
-    //initialize a module with usersService and usersRepository
     const module = await Test.createTestingModule({
       providers: [
         UsersService,
         { provide: UsersRepository, useFactory: mockUsersRepository },
         { provide: JwtService, useFactory: mockJwtService },
+        // { provide: mockBcrypt, useFactory: mockBcrypt },
       ],
     }).compile();
 
     usersService = module.get(UsersService);
     usersRepository = module.get(UsersRepository);
+    // userBcrypt = module.get(mockBcrypt);
   });
 
   describe('getUsers', () => {
     it('calls UsersRepository.getUsers and returns the result', async () => {
-      expect(usersRepository.getUsers).not.toHaveBeenCalled();
       usersRepository.getUsers.mockResolvedValue('someValue');
-      const result = await usersService.getUsers(null, null);
-      expect(usersRepository.getUsers).toHaveBeenCalled();
+      const result = await usersService.getUsers(null, null, null);
+
       expect(result).toEqual('someValue');
     });
   });
@@ -64,41 +64,49 @@ describe('UsersService', () => {
   describe('getSingleUser', () => {
     it('calls UsersRepository.getSingleUser and returns the result', async () => {
       usersRepository.getSingleUser.mockResolvedValue(mockUser);
-      const result = await usersService.getSingleUser('someId');
+      const result = await usersService.getSingleUser('someId', null);
+
       expect(result).toEqual(mockUser);
     });
 
     it('calls UsersRepository.getSingleUser and handles an error', async () => {
       usersRepository.getSingleUser.mockResolvedValue(null);
-      expect(usersService.getSingleUser('someId')).rejects.toThrow(
+
+      expect(usersService.getSingleUser('someId', null)).rejects.toThrow(
         NotFoundException,
       );
     });
   });
 
-  describe('addUser', () => {
-    it('calls UsersRepository.addUser and returns the result', async () => {
-      usersRepository.addUser.mockResolvedValue(mockUser);
-      const result = await usersService.addUser(null);
+  describe('createUser', () => {
+    it('calls UsersRepository.createUser and returns the result', async () => {
+      usersRepository.createUser.mockResolvedValue(mockUser);
+      const result = await usersService.createUser(null);
+
       expect(result).toEqual(mockUser);
     });
 
-    it('calls UsersRepository.addUser and handles the error', async () => {
-      usersRepository.addUser.mockResolvedValue(null);
-      expect(usersService.addUser(null)).rejects.toThrow(BadRequestException);
+    it('calls UsersRepository.createUser and handles the error', async () => {
+      usersRepository.createUser.mockResolvedValue(null);
+
+      expect(usersService.createUser(null)).rejects.toThrow(
+        BadRequestException,
+      );
     });
   });
 
   describe('updateUser', () => {
     it('calls UsersRepository.updateUser and returns the result', async () => {
       usersRepository.updateUser.mockResolvedValue(mockUser);
-      const result = await usersService.updateUser('someId', null);
+      const result = await usersService.updateUser('someId', null, null);
+
       expect(result).toEqual(mockUser);
     });
 
     it('calls UsersRepository.updateUser and handles the error', async () => {
       usersRepository.updateUser.mockResolvedValue(null);
-      expect(usersService.updateUser('someId', null)).rejects.toThrow(
+
+      expect(usersService.updateUser('someId', null, null)).rejects.toThrow(
         NotFoundException,
       );
     });
@@ -107,24 +115,26 @@ describe('UsersService', () => {
   describe('deleteUser', () => {
     it('calls UsersRepository.deleteUser and deletes the user', async () => {
       usersRepository.deleteUser.mockResolvedValue('user deleted');
-      const result = await usersService.deleteUser('someId');
+      const result = await usersService.deleteUser('someId', null);
+
       expect(result).toEqual('user deleted');
     });
 
     it('calls UsersRepository.deleteUser and handles the error', async () => {
       usersRepository.deleteUser.mockResolvedValue(null);
-      expect(usersService.deleteUser('someId')).rejects.toThrow(
+
+      expect(usersService.deleteUser('someId', null)).rejects.toThrow(
         NotFoundException,
       );
     });
   });
 
-  //   describe('login', () => {
-  //     it('calls UsersRepository.findone and checks if the user is valid', async () => {
-  //       usersRepository.findOne.mockResolvedValue(mockLogin);
-  //       mockBcrypt.compare.mockResolvedValue
-  //       const result = await usersService.login(mockLogin);
-  //       expect(result).toEqual('accessToken');
-  //     });
+  // describe('login', () => {
+  //   it('calls UsersRepository.findone and checks if the user is valid', async () => {
+  //     usersRepository.findOne.mockResolvedValue('valid');
+  //     userBcrypt.compare.mockResolvedValue('valid')
+  //     const result = await usersService.login(mockLogin);
+  //     expect(result).toEqual('accessToken');
   //   });
+  // });
 });
